64b757a1620ab1654fca9cc1986022d5e7181bac
spectrozinho <41579139+spectrozinho@users.noreply.github.com>
2018-07-25 03:28:25 -0300

    Create Inicializador.sh

---
+++ Inicializador.sh
@@ -0,0 +1,39 @@
+Sceipt feito por Spectro hacking
+# pessoas com permisoes de uso
+                                                                   # Meu canal = Spectro Hacking
+# https://www.youtube.com/channel/UCAmRS0i3PL2hlSNf8zpuKGg
+#
+# Psico Hacking
+# https://www.youtube.com/channel/UC3_D0-3q9z-O0yUkZfqPA-g
+
+apt install update && apt install upgrade
+
+apt install curl
+
+                                                                   
+clear
+
+
+
+echo " #################################################### "
+echo " #                                                  # "
+echo " #           \033[0;31mMetasploit auto installer\033[0;31m             # "
+echo " #                                                  # "
+echo " #            Ccoded by: spectro hacking            # "
+echo " #     Youtube:m/channel/UCAmRS0i3PL2hlSNf8zpuKGg   # "
+echo " #                                                  # "
+echo " #################################################### "
+
+
+
+
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+y
+
+
+chmod +x metasploit.sh
+
+./metasploit.sh
+~
+~
